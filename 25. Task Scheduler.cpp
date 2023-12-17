f8e659949356ae5aaddcc0a0d2dffc509930fd80
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2023-12-17 17:54:03 +0530

    Update and rename 24. Task Scheduler.cpp to 25. Task Scheduler.cpp

--- 25. Task Scheduler.cpp
+++ 25. Task Scheduler.cpp
@@ -1,3 +1,13 @@
+//Watch for understanding : https://www.youtube.com/watch?v=eGf-26OTI-A
+// Run it on the example : tasks = ["A","A","A","B","B","B"], n = 2
+
+
+
+//Q : 
+//Given a characters array tasks, representing the tasks a CPU needs to do, where each letter represents a different task. Tasks could be done in any order. Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
+//However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
+//Return the least number of units of times that the CPU will take to finish all the given tasks.
+
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
