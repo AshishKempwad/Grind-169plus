3d1198ffafbaa8553c59c933a1850f5d4ca697b9
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2023-12-19 23:24:57 +0530

    Update 39. Number of Islands.cpp

--- 39. Number of Islands.cpp
+++ 39. Number of Islands.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    bool isValid(string s) {
 
-        stack<char>st;
-        int n = s.size();
+    int dx[4] = {-1,0,0,1};
+    int dy[4] = {0,1,-1,0};
+
+
+    void dfs(vector<vector<char>>& grid, int x, int y, int n, int m){
+        grid[x][y]='2';
+
+        for(int k=0;k<4;k++){
+            int newx = x + dx[k];
+            int newy = y + dy[k];
+
+            if(newx >= 0 && newx < n && newy >= 0 && newy < m && grid[newx][newy]=='1'){
+                dfs(grid,newx,newy,n,m);
+            } 
+        }
+        
+    }
+
+
+    int numIslands(vector<vector<char>>& grid) {
+
+        int count = 0;
+        int n = grid.size();
+        int m = grid[0].size();
 
         for(int i=0;i<n;i++){
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
-                st.push(s[i]);
-            }
-            else{
-                if(st.empty()){
-                    return false;
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){                  
+                    dfs(grid,i,j,n,m);
+                    count++;
                 }
-                else if((s[i]=='}' && st.top()!='{') ||
-                        (s[i]==']' && st.top()!='[') ||
-                         (s[i]==')' && st.top()!='(') ){
-                             return false;
-                         }else{
-                             st.pop();
-                         }
             }
         }
 
-        if(st.empty()){
-            return true;
-        }
-
-        return false;
-
+        return count;
+        
     }
 };
+
+
