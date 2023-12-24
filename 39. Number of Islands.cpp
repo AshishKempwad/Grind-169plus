144449d4d95160dbdf423ba9232e61bcd6483754
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2023-12-24 13:36:06 +0530

    BFS Number of Islands.cpp

--- 39. Number of Islands.cpp
+++ 39. Number of Islands.cpp
@@ -40,4 +40,49 @@ public:
     }
 };
 
+---------------------------------------------------------------------------------------------------------------------
 
+   // BFS
+
+    class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        int count=0;
+        queue<pair<int,int>>q;
+        vector<int>dx ={1,0,0,-1};
+        vector<int>dy ={0,1,-1,0}; 
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && vis[i][j]==false){
+                    count++;
+                    q.push({i,j});
+                    vis[i][j]=true;
+
+                    while(!q.empty()){
+                        int x = q.front().first;
+                        int y = q.front().second;
+                        q.pop();
+
+                        for(int k=0;k<4;k++){
+                            int newx = x + dx[k];
+                            int newy = y + dy[k];
+
+                            if(newx >= 0 && newx <n && newy >= 0 && newy < m && grid[newx][newy]=='1' && vis[newx][newy]==false){
+                                vis[newx][newy]=true;
+                                q.push({newx,newy});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
+        
+    }
+};
